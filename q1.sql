a08efc72a64d7869e7069e5f5e6f70351f9ff583
danimal141 <hideaki.ishii1204@gmail.com>
2024-03-10 10:42:08 +0900

    Update and finish

--- q1.sql
+++ q1.sql
@@ -1,1 +1,10 @@
-SELECT gender, COUNT(*)  FROM users  WHERE NOT is_deleted = 1 GROUP BY gender;
+SELECT
+	user_id,
+	MAX(order_date) AS last_order_date,
+	COUNT (DISTINCT order_date) AS freq,
+	SUM(price) AS sum_price
+FROM orders
+INNER JOIN products ON orders.order_product_id = products.product_id
+WHERE is_canceled = 0 AND user_id IS NOT NULL
+GROUP BY user_id
+
